6ea3a9e59f217338fee043dc7ca371466b1e9fcb
csheahan <chris.sheahan@live.com>
2016-04-16 20:54:30 -0500

    add arguments for messages and time delay
    custom messages(-m and --message) and custom time delay between toast
    messages (-d and --message_time_delay)

--- README.md
+++ README.md
@@ -3,6 +3,23 @@
 A simple program to send desktop toast notifications to remind you to fix your
 posture.
 
+### Usage
+
+```
+$ python posture.py --help
+usage: posture.py [-h] [-m msg] [-d mm:ss]
+
+A simple python script to remind you to fix your posture
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -m msg, --message msg
+                        Custom message. Default is 'Fix your posture'
+  -d mm:ss, --message_time_delay mm:ss
+                        Delay in between toast messages in the format
+                        'mm:ss'.Default is '30:00'
+```
+
 ### Supported Operating Systems
 
 - Windows (10)
--- posture.py
+++ posture.py
@@ -1,13 +1,52 @@
+#!/usr/bin/env python
+
+import argparse
+import re
 import sys
 import time
 from modules.windows import windows_toast as windows
 
 def main():
+  desc = "A simple python script to remind you to fix your posture"
+  parser = argparse.ArgumentParser(description=desc)
+
+  # Optional Arguments (Flags)
+  parser.add_argument(
+    "-m",
+    "--message",
+    type=str,
+    help="Set a custom message. Default is 'Fix your posture'",
+    metavar="msg",
+    default="Fix your posture"
+  )
+
+  parser.add_argument(
+    "-d",
+    "--message_time_delay",
+    type=str,
+    help=("Delay in between toast messages in the format 'mm:ss'."
+          "Default is '30:00'"),
+    metavar="mm:ss",
+    default="30:00"
+  )
+
+  args = parser.parse_args()
+
+  # Ensure time is of the form "mm...mm:ss" where the there is at least 1
+  # number for the second and at least 1 number for the minute
+  if (not re.match(r"\b\d+:[0-6]?\d", args.message_time_delay)):
+    print("The time delay must be of the form 'mmm...mm:ss")
+    print("\twhere there is at least 1 number for the minute and second")
+    print("\twhere the first second digit, if 2 are present, is 1 through 6")
+    print("\twhere only digits are used")
+
+    exit(1)
+
   toast_function = None
-  toast_message = "Fix your posture"
+  toast_message = args.message
   toast_time = 5
-  second_delay = 0
-  minute_delay = 30
+  second_delay = int(args.message_time_delay.split(":")[1])
+  minute_delay = int(args.message_time_delay.split(":")[0])
 
   if (sys.platform.startswith("linux")):
     # The OS is a form of Linux
