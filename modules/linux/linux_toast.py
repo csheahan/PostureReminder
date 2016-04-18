43e7e8c8e194d3bd57755e2a76149a9d1b52b55e
csheahan <chris.sheahan@live.com>
2016-04-18 18:00:39 -0500

    Fix issue of linux/windows incombatability

--- modules/linux/linux_toast.py
+++ modules/linux/linux_toast.py
@@ -1,5 +1,9 @@
-def sendToast(message, time):
+import sys
+
+if (sys.platform.startswith("linux")):
   from gi.repository import Notify
+
+def sendToast(message, time):
   Notify.init("Posture Reminder")
   Notify.Notification.new(message).show()
 
