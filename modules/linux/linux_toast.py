a4edf489827ed9adda34c281338f9fb5135e38c7
csheahan <chris.sheahan@live.com>
2016-04-19 14:23:08 -0500

    add guards to os specific functions and imports

--- modules/linux/linux_toast.py
+++ modules/linux/linux_toast.py
@@ -4,7 +4,8 @@ if (sys.platform.startswith("linux")):
   from gi.repository import Notify
 
 def sendToast(message, time):
-  Notify.init("Posture Reminder")
-  Notify.Notification.new(message).show()
+  if (sys.platform.startswith("linux")):
+    Notify.init("Posture Reminder")
+    Notify.Notification.new(message).show()
 
-  Notify.uninit()
+    Notify.uninit()
--- modules/windows/windows_toast.py
+++ modules/windows/windows_toast.py
@@ -1,16 +1,18 @@
 from subprocess import call
+import sys
 
 def sendToast(message,
               time,
               ps_path=r".\modules\windows\send_toast_message.ps1"):
-  call(["powershell.exe",
-        "-noprofile",
-        "-executionpolicy",
-        "bypass",
-        "-file",
-        ps_path,
-        "-message",
-        message])
+  if (sys.platform == "win32"):
+    call(["powershell.exe",
+          "-noprofile",
+          "-executionpolicy",
+          "bypass",
+          "-file",
+          ps_path,
+          "-message",
+          message])
 
 if __name__ == "__main__":
   send_toast("cat", 5, r".\send_toast_message.ps1")
